4da5f328379965941390377b68120439a7ee11c0
NimraFatima <farimanim16@gmail.com>
2024-01-09 22:50:50 -0600

    js

---
+++ app.js
@@ -0,0 +1,10 @@
+var dates = document.getElementById("dates");
+var years = document.getElementById("years");
+var months = document.getElementById("months");
+var days = document.getElementById("days");
+function calculateAge(){
+    var dateOfBirth = new Date(dates.value).getTime();
+    var now = new Date().getTime();
+
+
+}
