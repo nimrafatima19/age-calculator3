b7b824afe8c38abfd3c4188b508becfadc781517
NimraFatima <farimanim16@gmail.com>
2024-01-10 22:29:58 -0600

    logics

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var days = document.getElementById("days");
 function calculateAge(){
     var dateOfBirth = new Date(dates.value).getTime();
     var now = new Date().getTime();
+    var diff = now - dateOfBirth;
+    var y = Math.floor(diff/(1000*60*60*24*365.25));
+    var m = Math.floor(diff%(1000*60*60*24*365.25)/(1000*60*60*24*(365.25/12)));
+    var d = Math.floor(diff%(1000*60*60*24*(365.25/12))/(1000*60*60*24));
+
+    years.innerHTML = y;
+    months.innerHTML = m;
+    days.innerHTML = d;
 
 
 }
