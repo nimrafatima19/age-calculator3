85188e2da381ac44ef3fd462c14a750a610cc00d
NimraFatima <farimanim16@gmail.com>
2024-01-10 22:50:50 -0600

    disable

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var dates = document.getElementById("dates");
 var years = document.getElementById("years");
 var months = document.getElementById("months");
 var days = document.getElementById("days");
+document.getElementById("days");
+dates.max = new Date().toISOString().split("T")[0];
 function calculateAge(){
     var dateOfBirth = new Date(dates.value).getTime();
     var now = new Date().getTime();
